78045aaad353fb43e385cf9e8cc11dca9ec682e7
Ray Li <rayli@Rays-MacBook-Air.local>
2013-09-15 08:39:01 -0400

    predictor works 2

--- tester.py
+++ tester.py
@@ -1,8 +1,18 @@
 import sys
-sys.path.insert(0, 'sentGen')
+sys.path.insert(0, 'Predictor')
 
-from generator import get_all_data, load_data, generate, generate_multiple
+from predictor import train, set_priors, predictor_func run_tests
 
-get_all_data ()
-for a in generate_multiple ("#yolo", 10):
-    print a
+print "Begin Training"
+train ()
+print "Training Complete"
+print "Setting Priors"
+set_priors ()
+print "Priors Set"
+run_tests (predictor_func)
+
+#from generator import get_all_data, load_data, generate, generate_multiple
+
+#get_all_data ()
+#for a in generate_multiple ("#yolo", 10):
+#    print a
