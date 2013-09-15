8aa341c6bd412cfeb431d7760f342ec38a0f7200
Kim Lister <retsilmik@gmail.com>
2013-09-14 20:23:31 -0400

    flask templates
    Here's the nice flask template for submitting hashtags

--- hello.py
+++ hello.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/other')
+def other_page():
+    return 'Page Change!'
+
 if __name__ == '__main__':
     app.run()
